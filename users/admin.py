cf54ca8bcd5d9fcec4c221525deec29e5a8a5c42
tamirho <tamirh1994@gmail.com>
2021-04-12 13:20:15 +0300

    fix admin.py

--- users/admin.py
+++ users/admin.py
@@ -14,6 +14,10 @@ class UserAdminConfig(UserAdmin):
                                  'birth_date', 'date_joined',
                                  'is_active', 'hobbies')}),
         ('Permissions', {'fields': ('is_staff', 'is_superuser',)}),
+        ('properties', {'fields': ('not_smoking', 'pets_allowed',
+                                   'air_conditioner', 'balcony',
+                                   'elevator', 'long_term',
+                                   'immediate_entry',)}),
     )
 
     add_fieldsets = (
@@ -22,7 +26,11 @@ class UserAdminConfig(UserAdmin):
             'fields': ('email', 'password1', 'password2',
                        'first_name', 'last_name', 'birth_date',
                        'is_staff', 'is_active',
-                       'is_superuser', 'hobbies'),
+                       'is_superuser', 'hobbies',
+                       'not_smoking', 'pets_allowed',
+                       'air_conditioner', 'balcony',
+                       'elevator', 'long_term',
+                       'immediate_entry',),
         }),
     )
 
