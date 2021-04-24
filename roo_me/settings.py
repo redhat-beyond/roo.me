9204df3c05328d9683e66e4e93d2a4d98ea09f3f
tamirho <tamirh1994@gmail.com>
2021-04-24 20:16:36 +0300

    Adding apartments app

--- roo_me/settings.py
+++ roo_me/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
     'users.apps.UsersConfig',
-    'contacts.apps.ContactsConfig'
+    'contacts.apps.ContactsConfig',
+    'apartments.apps.ApartmentsConfig',
 ]
 
 MIDDLEWARE = [
