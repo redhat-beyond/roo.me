3a2171ede25bbec456e18011318e4b2ec3a87930
Tamir Houri <65215909+tamirho@users.noreply.github.com>
2021-04-27 11:15:20 +0300

    Alter the url patterns (#82)
    - Alter the roo_me/url.py to use include() for all apps
    - Add urls.py to the relevant apps

--- roo_me/urls.py
+++ roo_me/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from main import views
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', include('main.urls')),
+    path('apartments/', include('apartments.urls')),
+    path('contacts/', include('contacts.urls')),
 ]
