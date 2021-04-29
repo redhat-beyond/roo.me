2e80ac29964215c0dcde79a9dec596bb9359a376
Micha546 <79044795+Micha546@users.noreply.github.com>
2021-04-29 21:01:19 +0300

    Move seeker (#91)
    * Moving Seeker model
    
    Moves Seeker to seekers app.
    
    Changed all imports to import Seeker from seekers instead of users.
    
    commented out 2 tests that assume there is data in the tables,
    because right now there is no data, ill add the data in the next PR
    and then ill include those tests as well.
    
    * Creating new migration files
    
    Ran makemigrations, all files are auto-made...
    
    **note:** before runnig vagrant provision/migrate make sure to
    delete db.sqlite3 !!

--- seekers/models.py
+++ seekers/models.py
@@ -1,3 +1,16 @@
-# from django.db import models
+from django.db import models
+from roo_me.settings import AUTH_USER_MODEL
 
-# Create your models here.
+
+class Seeker(models.Model):
+    base_user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    city = models.ForeignKey('apartments.City', on_delete=models.RESTRICT, related_name='city_seekers')
+    start_date = models.DateField()
+    min_rent = models.IntegerField()
+    max_rent = models.IntegerField()
+    num_of_roomates = models.IntegerField()
+    num_of_rooms = models.IntegerField()
+    about = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Seeker:{self.base_user}"
