ca72c92d785550fc9afd531aea1a42f174baec9f
Daniel Malky <DanielMy@mta.ac.il>
2021-05-04 19:56:05 -0200

    Adding the register template
    1) Creating the template using custom CSS.
    2) Creating a view and directory url.
    3) Creating a minor test to check validity.

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
 ]
