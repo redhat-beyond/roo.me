a696fcd7f534984fc2ad74cb0ecbb14f526d81fe
Daniel Malky <DanielMy@mta.ac.il>
2021-05-09 23:31:34 -0200

    Adding a view for the contact list
    1) Adding a url path to reach the contact list view.
    2) Adding a view that will redirect each user to its
       compatible contacts list page.
    3) Adding tests to insure a good redirect and load of the page.

--- contacts/tests.py
+++ contacts/tests.py
@@ -4,6 +4,80 @@ from django.contrib.auth import get_user_model
 from django.db import transaction, IntegrityError
 
 
+<<<<<<< HEAD
+=======
+@pytest.fixture
+def new_city():
+    city = City(cityName='Tel Aviv')
+    city.save()
+    return city
+
+
+@pytest.fixture
+def make_user():
+    def _make_user(email, f_name, l_name, b_date, passw):
+        user = User(
+            email=email,
+            first_name=f_name,
+            last_name=l_name,
+            birth_date=b_date,
+        )
+        user.set_password(passw)
+        user.save()
+        return user
+
+    return _make_user
+
+
+@pytest.fixture
+def make_seeker(new_city):
+    def _make_seeker(user, s_date, min_r, max_r, roomates, rooms, about):
+        seeker = Seeker(
+            base_user=user,
+            city=new_city,
+            start_date=s_date,
+            min_rent=min_r,
+            max_rent=max_r,
+            num_of_roomates=roomates,
+            num_of_rooms=rooms,
+            about=about
+        )
+        seeker.save()
+        return seeker
+
+    return _make_seeker
+
+
+@pytest.fixture
+def make_apartment(new_city):
+    def _make_apartment(user, addr, rent, roomates, rooms, s_date):
+        apartmant = Apartment(
+            owner=user,
+            city=new_city,
+            address=addr,
+            rent=rent,
+            num_of_roomates=roomates,
+            num_of_rooms=rooms,
+            start_date=s_date
+        )
+        apartmant.save()
+        return apartmant
+
+    return _make_apartment
+
+
+@pytest.fixture
+def sample_connection(make_seeker, make_apartment, make_user):
+    user1 = make_user("t1@m.com", "test1", "test", "1995-05-05", "testing")
+    seeker = make_seeker(user1, "2020-05-05", 100, 1000, 2, 2, "Hello")
+    user2 = make_user("t3@m.com", "test3", "test", "1995-05-05", "testing")
+    apartment = make_apartment(user2, "Hatotahim 5", 2500, 2, 3, "2020-05-05")
+    con = Connection(seeker=seeker, apartment=apartment)
+    con.save()
+    return con
+
+
+>>>>>>> 69291ea (Adding a view for the contact list)
 @pytest.mark.django_db
 def test_add_connection(sample_connection):
     assert sample_connection is not None
@@ -91,3 +165,27 @@ def test_get_approved_connections(sample_connection):
     assert sample_connection.get_status == 'Approved'
     approved = Connection.get_connections_by_user(sample_connection.seeker.base_user, ConnectionType.APPROVED)
     assert approved.count() == 1
+
+
+@pytest.mark.django_db
+def test_seeker_redirect_to_contacts_page(sample_connection, client):
+    seeker_email = "t1@m.com"
+    seeker_pass = "testing"  # credentials for sample_connection.seeker
+    client.login(email=seeker_email, password=seeker_pass)
+    response = client.get('/contacts/')
+    assert response.status_code == 200
+    logged_user = response.wsgi_request.user
+    assert logged_user == sample_connection.seeker.base_user
+    assert b"This connection request has yet to be approved" in response.content
+
+
+@pytest.mark.django_db
+def test_owner_redirect_to_contacts_page(sample_connection, client):
+    owner_email = "t3@m.com"
+    owner_pass = "testing"  # credentials for sample_connection.apartment
+    client.login(email=owner_email, password=owner_pass)
+    response = client.get('/contacts/')
+    assert response.status_code == 200
+    logged_user = response.wsgi_request.user
+    assert logged_user == sample_connection.apartment.owner
+    assert b"You can approve or decline this connection" in response.content
