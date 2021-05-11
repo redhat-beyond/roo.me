03f12920f2363512f65aa65f6a0357b2ac9169ce
Nadav <nadavsuliman@gmail.com>
2021-05-11 14:29:38 +0300

    Update routing at homepage for seekers and owners
    `views.py` now routes the user based on his account type.
    This is just the skeleton, we still need to add the functions which we will route to.

--- main/views.py
+++ main/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    if request.user.is_authenticated:
+    if not request.user.is_authenticated:
+        return render(request, 'main/landing_page.html')
+    elif request.user.is_seeker:
+        ''' TODO: delete current render and create a render_seeker_home
+            function that will render the homepage for the seeker.'''
         return render(request, 'main/home.html')
-    return render(request, 'main/landing_page.html')
+    elif request.user.is_owner:
+        ''' TODO: delete current render and create a render_owner_home
+            function that will render the homepage for the owner.'''
+        return render(request, 'main/home.html')
+    else:
+        return render(request, 'main/landing_page.html')
 
 
 def register(request):
