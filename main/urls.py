bd26bf27681d36dbf973f7fdecfb8ac40f23ea3d
Nadav <53123142+nadavsu@users.noreply.github.com>
2021-05-12 15:43:43 +0300

    Redirect logout page to landing page (#158)
    The logout page was not very useful. Now deprecated.
    Sorry, Daniel.

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='main/landing_page.html'), name='logout'),
 ]
