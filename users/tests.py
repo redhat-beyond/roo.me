0086e1aa3b2bdefbd3bb6181605f77a59c71280e
Micha Levy <themicha546@gmail.com>
2021-05-12 15:29:14 +0300

    Adding update forms for the user fields
    Added UserUpdateForm and used the PasswordChangeForm from Django
    to change the users password.
    Added users url to the urls in roo_mo.urls.
    Added html for the user update and for the password change
    Added some tests to check that everything works

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import transaction, IntegrityError
-from .forms import UserCreationForm
+from .forms import UserCreationForm, UserUpdateForm
+from django.urls import reverse
 import pytest
 
 
@@ -103,7 +104,7 @@ def test_is_owner(seeker_model, apartment_model):
         ('email@address.com', 'first_name', 'last_name', '1900-1-1', 'hello321hi', 'hi123hello', False),
     ])
 @pytest.mark.django_db
-def test_form_validity(email, first_name, last_name, birth_date, password1, password2, validity):
+def test_creation_form_validity(email, first_name, last_name, birth_date, password1, password2, validity):
     form = UserCreationForm(data={
         'email': email,
         'first_name': first_name,
@@ -119,3 +120,45 @@ def test_form_validity(email, first_name, last_name, birth_date, password1, pass
 @pytest.mark.django_db
 def test_valid_form_is_valid(valid_user_creation_form):
     assert valid_user_creation_form.is_valid
+
+
+@pytest.mark.parametrize(
+    'email, first_name, last_name, birth_date, validity',
+    [
+        ('email@address.com', 'first_name', 'last_name', '1900-1-1', True),
+        ('', 'first_name', 'last_name', '1900-1-1', False),
+        ('email@address.com', '', 'last_name', '1900-1-1', False),
+        ('email@address.com', 'first_name', '', '1900-1-1', False),
+        ('email@address.com', 'first_name', 'last_name', '', False),
+        ('non-valid-email', 'first_name', 'last_name', '1900-1-1', False),
+    ])
+@pytest.mark.django_db
+def test_update_form_validity(email, first_name, last_name, birth_date, validity):
+    form = UserUpdateForm(data={
+        'email': email,
+        'first_name': first_name,
+        'last_name': last_name,
+        'birth_date': birth_date,
+    })
+
+    assert form.is_valid() is validity
+
+
+@pytest.mark.django_db
+def test_update_user_view(client, user_model):
+    url = reverse('update-user')
+    response = client.get(url)
+    assert response.status_code == 302
+    client.login(email='email@address.com', password='password')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_change_password_view(client, user_model):
+    url = reverse('change-password')
+    response = client.get(url)
+    assert response.status_code == 302
+    client.login(email='email@address.com', password='password')
+    response = client.get(url)
+    assert response.status_code == 200
---
+++ users/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
+from .forms import UserUpdateForm
+
+
+@login_required
+def update_user(request):
+    if request.method == 'POST':
+        update_settings_form = UserUpdateForm(request.POST, instance=request.user)
+        if update_settings_form.is_valid():
+            update_settings_form.save(commit=True)
+            return redirect('update-user')
+    else:
+        update_settings_form = UserUpdateForm(instance=request.user)
+    return render(request, 'users/update.html', {
+        'settings_form': update_settings_form
+    })
+
+
+@login_required
+def password_change(request):
+    if request.method == 'POST':
+        password_change_form = auth_views.PasswordChangeForm(user=request.user, data=request.POST)
+        if password_change_form.is_valid():
+            password_change_form.save()
+            update_session_auth_hash(request, password_change_form.user)
+            return redirect('update-user')
+    else:
+        password_change_form = auth_views.PasswordChangeForm(user=request.user)
+    return render(request, 'users/change-password.html', {
+        'form': password_change_form
+    })
