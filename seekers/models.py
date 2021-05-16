77a46a67541fa940e0601b3bf899d94283a891ee
Nadav <nadavsuliman@gmail.com>
2021-05-16 10:37:57 +0300

    Add function to get the matching apartments for seeker
    `get_matched_apartments` returns an ordered QuerySet of apartments which match the preferences of the user, ordered by the matching score creating by the ranking algorithm.
    
    Note: `get_all_relevant_apartments` is a temporary function which was written in order to create this feature. Once the search feature is created, this function must be deleted, as we'll use the function in the search feature.

--- seekers/models.py
+++ seekers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from roo_me.settings import AUTH_USER_MODEL
+from apartments.models import Apartment
 
 
 class Seeker(models.Model):
@@ -14,3 +15,25 @@ class Seeker(models.Model):
 
     def __str__(self):
         return f"Seeker:{self.base_user}"
+
+    def get_matched_apartments(self):
+        apartments = self.get_all_relevant_apartments()
+        return sorted(
+            apartments,
+            key=lambda apt: apt.owner.get_matching_score(self.base_user),
+            reverse=True
+        )[:10]
+
+    '''TODO: Delete this function and use the search function from the search application instead.
+    This is a temporary function - it was only written in order to develop the feature.
+    DO NOT USE once the search application is merged.'''
+    def get_all_relevant_apartments(self):
+        return Apartment.objects.filter(**{
+                'is_relevant': True,
+                'city': self.city,
+                'rent__lte': self.max_rent,
+                'rent__gte': self.min_rent,
+                'num_of_roomates__lte': self.num_of_roomates,
+                'num_of_rooms': self.num_of_rooms,
+                'start_date__lte': self.start_date,
+            })
