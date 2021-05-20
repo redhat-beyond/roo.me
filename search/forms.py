880aee4252300c3451acec75065881ab0ee77d4c
AmitAharoni <aharoni94@gmail.com>
2021-05-20 20:19:14 +0300

    Correct forms labels
    -Added format for the date label.
    -some of the forms labels were shortcuts, corrected them.
    examples:
    min - Minimum.
    num - Number.

--- search/forms.py
+++ search/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from seekers.models import Seeker
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator    
 
 
 class SearchForm(forms.ModelForm):
-    min_rent = forms.IntegerField(validators=[MinValueValidator(limit_value=0)])
-    max_rent = forms.IntegerField(validators=[MinValueValidator(limit_value=0)])
-    num_of_roomates = forms.IntegerField(validators=[MinValueValidator(limit_value=0)])
-    num_of_rooms = forms.IntegerField(validators=[MinValueValidator(limit_value=0)])
+    min_rent = forms.IntegerField(label='Minimum rent', validators=[MinValueValidator(limit_value=0)])
+    max_rent = forms.IntegerField(label='Maximum rent', validators=[MinValueValidator(limit_value=0)])
+    num_of_roomates = forms.IntegerField(label='Number of roomates', validators=[MinValueValidator(limit_value=0)])
+    num_of_rooms = forms.IntegerField(label='Number of rooms', validators=[MinValueValidator(limit_value=0)])
+    start_date = forms.DateField(label='Start date [YYYY-MM-DD]')
 
     class Meta:
         model = Seeker
